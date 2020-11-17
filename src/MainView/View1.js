6568ae059e6e1e44eee73f34ed81b2d230719d01
lebcoco <leee24638@gmail.com>
2020-11-17 15:59:24 +0900

    Modify 배경이미지 수정

--- src/MainView/View1.js
+++ src/MainView/View1.js
@@ -2,20 +2,29 @@ import React, {Component} from "react";
 import {Grid, Header, Segment} from "semantic-ui-react";
 import Button from '@material-ui/core/Button';
 
+// import bgImage1 from '../../public/main1.png';
+
 import './mainView.css';
 
 
 const styles = {
   
   bgImage: {
-      height: '1080px',
-      backgroundRepeat: 'no-repeat',
-      backgroundImage: `url(${"./main1.PNG"})`
+      backgroundImage: `url(${"./main1.PNG"})`,
+      height: '100%',
+      width: '100%',
+      backgroundRepeat: 'no-repeat',    
+      padding: "10em 12em 10em 12em"
+      // position: 'absolute',
+      // left: '50%',
+      // marginLeft: '-50px',
+     
   },
   mainFont:{
     fontFamily: "NanumSquare",
-    fontSize: "60px",
-    paddingBottom: "30px"
+    fontSize: "50px",
+    paddingBottom: "30px",
+    // backgroundColor: "brown"
   },
   subFont:{
     fontFamily: "NanumSquare",
@@ -34,33 +43,27 @@ export default class View1 extends Component {
   
   render() {
     return (
-      <div style={styles.bgImage}>
-        <Segment style={{padding: "10em 12em 10em 12em"}}vertical="vertical">
-        <Grid container="container" stackable="stackable" verticalAlign="middle">
-            <Grid.Row>
-              <Grid.Column width={8} style={{paddingTop: "100px"}}>
+        // <Segment vertical="vertical" style={styles.bgImage}>
+        <Segment vertical="vertical" style={styles.bgImage}>
+        <Grid container="container" stackable="stackable" verticalAlign="middle" >
+            <Grid.Row>              
+              <Grid.Column width={8} 
+              style={{paddingTop: "100px"} }>
+                
                 <Header style={styles.mainFont}>
                   기억은 사라지지만<br />
                   기록은 남아있다.
-                </Header>
+                </Header>                
                 <p style={styles.subFont}>
                 내 하루를 온전히 기억하고 싶다면, 나만의 데이터셋을 만들고 싶다면<br />daily 101으로 기록하세요.
-
-                </p>
+                </p>                
+                <Button variant="text" style={styles.buttonFont}>자세히 알아보기 ></Button>
               </Grid.Column>
-              
-              <Grid.Column floated="right" width={10}>
+              <Grid.Column width={8} style={{paddingTop: "100px"}}>             
               </Grid.Column>
-            </Grid.Row>
-
-            <Grid.Row>
-              <Grid.Column>
-              <Button variant="text" style={styles.buttonFont}>자세히 알아보기 ></Button>
-              </Grid.Column>
-            </Grid.Row>
+              </Grid.Row>
           </Grid>
         </Segment>
-      </div>
     );
   }
 }
